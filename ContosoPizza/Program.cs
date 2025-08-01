d7783767dd3a4b3ec556e2e5c66cb8d4f7790608
Celso Pires <celso.pires1999@gmail.com>
2025-08-01 11:09:09 +0000

    Inserting

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+// var builder = WebApplication.CreateBuilder(args);
+// var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
 
-app.Run();
+// app.Run();
+
+using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new();
+
+Product veggieSpecial = new()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99m
+};
+
+context.Products.Add(veggieSpecial);
+
+Product deluxeMeat = new()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99m
+};
+
+context.Add(deluxeMeat);
+
+context.SaveChanges();
